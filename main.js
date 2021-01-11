59ec3eff55d0f780fc7ced8fbad73933cff3c2a9
NadhemBacha <nadhembecha@gmail.com>
2021-01-11 10:24:58 +0100

    Project01

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function showValue(num){
+  document.getElementById('output').value+=num
+}
+function operation(){
+  var number=document.getElementById('output').value
+  var number1=eval(number)
+  document.getElementById('output').value= number1
+}
+function Reset(){
+  document.getElementById('output').value =''
+}
+function removeOne(){
+  var rem1=document.getElementById('output').value
+  var str="";
+  var i =0;
+  while(i<rem1.length-1){
+    str+=rem1[i];
+    i++;
+  }
+  document.getElementById('output').value=str
+}
+function pow2(){
+  var total= document.getElementById('output').value;
+  total*= total
+  document.getElementById('output').value=total
+}
